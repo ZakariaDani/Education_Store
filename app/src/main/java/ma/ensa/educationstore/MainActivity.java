37c82e57f87269faa34a16a288f4d014de071ce0
Zakaria Dani <zakaria.dani2015@gmail.com>
2022-11-16 21:31:24 +0100

    first commit

---
+++ app/src/main/java/ma/ensa/educationstore/MainActivity.java
@@ -0,0 +1,14 @@
+package ma.ensa.educationstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
